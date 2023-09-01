0e29e06165adb049cc216a4a69fd952be83a26bc
Kanishk Reddy <107420043+predator3813@users.noreply.github.com>
2023-09-01 19:17:26 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+       'animation': "url('img/animation.gif')",
+      }
+    },
+  },
+  plugins: [],
+}
+
